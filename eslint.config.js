f5fc3f366073639bcf85e258325c8271f3216274
VesperQuartz <lilbrown3000@gmail.com>
2025-10-30 09:10:00 +0100

    some fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,17 @@
 /* eslint-env node */
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+const reactCompiler = require("eslint-plugin-react-compiler");
 
 module.exports = defineConfig([
-  expoConfig,
-  {
-    ignores: ['dist/*'],
-  },
-  {
-    rules: {
-      'react/display-name': 'off',
-    },
-  },
+	expoConfig,
+	reactCompiler.configs.recommended,
+	{
+		ignores: ["dist/*"],
+	},
+	{
+		rules: {
+			"react/display-name": "off",
+		},
+	},
 ]);
